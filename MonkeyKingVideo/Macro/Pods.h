fe737da90fa07274ee427533a919af308a66c553
Jobs <lg295060456@gmail.com>
2020-12-08 20:13:00 +0700

    no message

--- MonkeyKingVideo/Macro/Pods.h
+++ MonkeyKingVideo/Macro/Pods.h
@@ -9,63 +9,226 @@
 #ifndef Pods_h
 #define Pods_h
 
-//#import "BSYTextFiled.h"
-//#import "NJKWebViewProgress.h"
-//#import "PGBanner.h"
-//#import "PINOperation.h"
-//#import "PINRemoteImage.h"//支持带标记的图片后处理。对于同一张图片，当需要不同的后处理方式时（a 界面需要正圆角，b 界面需要小幅度的圆角），尤为有用 https://juejin.im/post/5a96a9b4f265da4e7f35d24e#heading-4
-//#import "POP.h"
-//#import "FBShimmering.h"
-//#import <AsyncDisplayKit/AsyncDisplayKit.h>
-//#import "ZXingObjC.h"
-//#import "AwesomeMenu.h"
-//#import "JJStockView.h"
-//#import "LBXScanView.h"
-//#import "Debugo.h"
-//#import "JPImageresizerView.h"
-
-#import <TXFileOperation.h>
-#import "TWPageViewController.h"//https://www.jianshu.com/p/25b1a3888bb8
-#import "AFNetworking.h"
-#import "AYCheckManager.h"
-#import "BEMCheckBox.h"
-#import "DDProgressView.h"
-#import "FMDB.h"
-#import "HCSStarRatingView.h"
-#import <IQKeyboardManager/IQKeyboardManager.h>
-#import <Masonry/Masonry.h>
-//#import "MBProgressHUD.h"
-#import "MBProgressHUD+WJExtension.h"
-#import "MJExtension.h"
-#import "MJRefresh.h"
-#import "OpenUDID.h"
-#import "Reachability.h"//检查联网情况
-#import <ReactiveObjC.h>
-#import "SDWebImage-umbrella.h"//不支持图片后处理
-#import "TXScrollLabelView.h"
-#import "JXCategoryView.h"
-#import "GKNavigationBar.h"
-#import <HBDNavigationBar/HBDNavigationController.h>
-
-#import "TZImagePickerController.h"
-
-#import <YYKit/YYKit.h>
-#import "BRPickerView.h"
-#import <LYEmptyView/LYEmptyViewHeader.h>
-#import "GKLikeView.h"
-#import "UIScrollView+EmptyDataSet.h"
-#import <YXAlertController.h>
-#import "SZTextView.h"
-#import <LKDBHelper.h>
-#import "YQImageCompressTool.h"
-#import "JPImageresizerView-umbrella.h"
-#import <Lottie/Lottie.h>
-//#import "AWRichText.h"
-//#import "WMZBannerView.h"
-#import "JXPagerView.h"
+#if __has_include(<JXPagerListRefreshView/JXPagerListRefreshView.h>)
+#import <JXPagerListRefreshView/JXPagerListRefreshView.h>
+#else
 #import "JXPagerListRefreshView.h"
-#import "PPBadgeView.h"
-#import "SPAlertController.h"
+#endif
 
+#if __has_include(<YQImageCompressTool/YQImageCompressTool.h>)
+#import <YQImageCompressTool/YQImageCompressTool.h>
+#else
+#import "YQImageCompressTool.h"
+#endif
+
+//#if __has_include(<LKDBHelper/LKDBHelper.h>)
+//#import <LKDBHelper/LKDBHelper.h>
+//#else
+//#import "LKDBHelper.h"
+//#endif
+
+#if __has_include(<TKCarouselView/TKCarouselView.h>)
+#import <TKCarouselView/TKCarouselView.h>
+#else
+#import "TKCarouselView.h"
+#endif
+
+#if __has_include(<SZTextView/SZTextView.h>)
+#import <SZTextView/SZTextView.h>
+#else
+#import "SZTextView.h"
+#endif
+
+#if __has_include(<DZNEmptyDataSet/UIScrollView+EmptyDataSet.h>)
+#import <DZNEmptyDataSet/UIScrollView+EmptyDataSet.h>
+#else
+#import "UIScrollView+EmptyDataSet.h"
+#endif
+
+#if __has_include(<GKLikeView/GKLikeView.h>)
+#import <GKLikeView/GKLikeView.h>
+#else
+#import "GKLikeView.h"
+#endif
+
+#if __has_include(<YYKit/YYKit.h>)
+#import <YYKit/YYKit.h>
+#else
+#import "YYKit.h"
+#endif
+//https://www.jianshu.com/p/25b1a3888bb8
+//#if __has_include(<TWPageViewController/TWPageViewController.h>)
+//#import <TWPageViewController/TWPageViewController.h>
+//#else
+//#import "TWPageViewController.h"
+//#endif
+
+#if __has_include(<TXScrollLabelView/TXScrollLabelView.h>)
+#import <TXScrollLabelView/TXScrollLabelView.h>
+#else
+#import "TXScrollLabelView.h"
+#endif
+
+#if __has_include(<DDProgressView/DDProgressView.h>)
+#import <DDProgressView/DDProgressView.h>
+#else
+#import "DDProgressView.h"
+#endif
+
+#if __has_include(<HCSStarRatingView/HCSStarRatingView.h>)
+#import <HCSStarRatingView/HCSStarRatingView.h>
+#else
+#import "HCSStarRatingView.h"
+#endif
+
+#if __has_include(<OpenUDID/OpenUDID.h>)
+#import <OpenUDID/OpenUDID.h>
+#else
+#import "OpenUDID.h"
+#endif
+
+#if __has_include(<FMDB/FMDB.h>)
+#import <FMDB/FMDB.h>
+#else
+#import "FMDB.h"
+#endif
+
+#if __has_include(<BEMCheckBox/BEMCheckBox.h>)
+#import <BEMCheckBox/BEMCheckBox.h>
+#else
+#import "BEMCheckBox.h"
+#endif
+
+#if __has_include(<AYCheckManager/AYCheckManager.h>)
+#import <AYCheckManager/AYCheckManager.h>
+#else
+#import "AYCheckManager.h"
+#endif
+
+#if __has_include(<TXFileOperation/TXFileOperation.h>)
+#import <TXFileOperation/TXFileOperation.h>
+#else
+#import "TXFileOperation.h"
+#endif
+
+#if __has_include(<YQImageCompressTool/YQImageCompressTool.h>)
+#import <YQImageCompressTool/YQImageCompressTool.h>
+#else
+#import "YQImageCompressTool.h"
+#endif
+
+#if __has_include(<TZImagePickerController/TZImagePickerController.h>)
+#import <TZImagePickerController/TZImagePickerController.h>
+#else
+#import "TZImagePickerController.h"
+#endif
+
+#if __has_include(<HBDNavigationBar/HBDNavigationController.h>)
+#import <HBDNavigationBar/HBDNavigationController.h>
+#else
+#import "HBDNavigationController.h"
+#endif
+
+#if __has_include(<AFNetworking/AFNetworking.h>)
+#import <AFNetworking/AFNetworking.h>
+#else
+#import "AFNetworking.h"
+#endif
+
+#if __has_include(<IQKeyboardManager/IQKeyboardManager.h>)
+#import <IQKeyboardManager/IQKeyboardManager.h>
+#else
+#import "IQKeyboardManager.h"
+#endif
+
+#if __has_include(<Masonry/Masonry.h>)
+#import <Masonry/Masonry.h>
+#else
+#import "Masonry.h"
+#endif
+
+#if __has_include(<MJExtension/MJExtension.h>)
+#import <MJExtension/MJExtension.h>
+#else
+#import "MJExtension.h"
+#endif
+
+#if __has_include(<MJRefresh/MJRefresh.h>)
+#import <MJRefresh/MJRefresh.h>
+#else
+#import "MJRefresh.h"
+#endif
+
+#if __has_include(<Reachability/Reachability.h>)
+#import <Reachability/Reachability.h>
+#else
+#import "Reachability.h"//检查联网情况
+#endif
+
+#if __has_include(<ReactiveObjC/ReactiveObjC.h>)
+#import <ReactiveObjC/ReactiveObjC.h>
+#else
+#import "ReactiveObjC.h"
+#endif
+
+#if __has_include(<SDWebImage/SDWebImage.h>)
+#import <SDWebImage/SDWebImage.h>
+#else
+#import "SDWebImage.h"
+#endif
+
+#if __has_include(<JXCategoryView/JXCategoryView.h>)
+#import <JXCategoryView/JXCategoryView.h>
+#else
+#import "JXCategoryView.h"
+#endif
+
+#if __has_include(<GKNavigationBar/GKNavigationBar.h>)
+#import <GKNavigationBar/GKNavigationBar.h>
+#else
+#import "GKNavigationBar.h"
+#endif
+
+#if __has_include(<BRPickerView/BRPickerView.h>)
+#import <BRPickerView/BRPickerView.h>
+#else
+#import "BRPickerView.h"
+#endif
+
+#if __has_include(<LYEmptyViewHeader/LYEmptyViewHeader.h>)
+#import <LYEmptyViewHeader/LYEmptyViewHeader.h>
+#else
+#import "LYEmptyViewHeader.h"
+#endif
+
+#if __has_include(<JPImageresizerView/JPImageresizerView.h>)
+#import <JPImageresizerView/JPImageresizerView.h>
+#else
+#import "JPImageresizerView.h"
+#endif
+
+#if __has_include(<lottie-ios/Lottie.h>)
+#import <lottie-ios/Lottie.h>
+#else
+#import "Lottie.h"
+#endif
+
+#if __has_include(<JXPagingView/JXPagerView.h>)
+#import <JXPagingView/JXPagerView.h>
+#else
+#import "JXPagerView.h"
+#endif
+
+#if __has_include(<PPBadgeView/PPBadgeView.h>)
+#import <PPBadgeView/PPBadgeView.h>
+#else
+#import "PPBadgeView.h"
+#endif
+
+#if __has_include(<SPAlertController/SPAlertController.h>)
+#import <SPAlertController/SPAlertController.h>
+#else
+#import "SPAlertController.h"
+#endif
 
 #endif /* Pods_h */
--- Podfile
+++ Podfile
@@ -55,6 +55,8 @@ def alert
   end
 # UI相关
 def ui
+  
+  pod 'TKCarouselView'
   pod 'SFProgressCircle'
   pod 'DZNEmptyDataSet'
   pod 'JDStatusBarNotification' # 网络提示--> 网络监听显示，主要是展示状态
@@ -95,7 +97,7 @@ def videoFunc
 # 一些功能性
 def func
   pod 'TXFileOperation' # 文件夹操作
-  pod 'TZImagePickerController' # 相册选择
+  pod 'TZImagePickerController'## 相册选择
   pod 'JPImageresizerView' # https://github.com/Rogue24/JPImageresizerView 一个专门裁剪图片、GIF、视频的轮子，简单易用，功能丰富（高自由度的参数设定、支持旋转和镜像翻转、蒙版、压缩等），能满足绝大部分裁剪的需求。
   pod 'lottie-ios', '~> 2.5.3' # 这是OC终极版本
   pod 'YQImageCompressor' # https://github.com/976431yang/YQImageCompressor iOS端简易图片压缩工具
